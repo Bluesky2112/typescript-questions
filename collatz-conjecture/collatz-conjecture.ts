96636fc6e3b0f204c98968c4b7b328ed5e186e84
Hieu Do <xlu.untitled@gmail.com>
2019-10-22 21:36:34 +0700

    Add collatz conjecture

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,4 +1,23 @@
 class CollatzConjecture {
+  static steps(n: number, count: number = 0): number {
+    if (n <= 0) {
+      throw new Error('Only positive numbers are allowed');
+    }
+
+    if (n === 1) {
+      return count;
+    }
+
+    // Divide by 2 if even
+    // (n1) = n0 / 2
+    // 3n + 1 if odd
+    // n1 = 3n0 + 1
+    if (n % 2 === 0) {
+      return this.steps(n / 2, ++count);
+    }
+
+    return this.steps(3 * n + 1, ++count);
+  }
 }
 
-export default CollatzConjecture
+export default CollatzConjecture;
